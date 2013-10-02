7b1953c89db8c94c240712e09ea34a192d4611ce
Ryan Zec <code@ryanzec.com>
2013-10-02 19:50:07 -0400

    Inline documentation updates

--- new-window.js
+++ new-window.js
@@ -1,24 +1,39 @@
 /**
- * Will open a new window when element is clicked
+ * # New Window
+ *
+ * Opens a new window with window.open().
+ *
+ * ```html
+ * <button nag-new-window="http://www.example/com">new window</button>
+ * ```
+ *
+ * If you need to be able to access the new window whether through javascript or for testing, you can pass the window-name attribute.
+ *
+ * ```html
+ * <button nag-new-window="http://www.example/com" window-name="my-window">new window</button>
+ * ```
  *
  * @module nag.newWindow
  * @ngdirective nagNewWindow
+ *
+ * @nghtmlattribute {string} nag-new-window The url of where the new window should go
+ * @nghtmlattribute {string} [window-name] The name of the window that case be used to access it through javascript
  */
 angular.module('nag.newWindow', [])
 .directive('nagNewWindow', [
-    function() {
+  function() {
+    return {
+      restrict: 'A',
+      priority: 0,
+      compile: function(element, attributes, transclude) {
         return {
-            restrict: 'A',
-            priority: 0,
-            compile: function(element, attributes, transclude) {
-                return {
-                    post: function(scope, element, attributes) {
-                        element.click(function(event) {
-                            window.open(attributes.nagNewWindow, attributes.windowName);
-                        })
-                    }
-                };
-            }
-        }
+          post: function(scope, element, attributes) {
+            element.click(function(event) {
+              window.open(attributes.nagNewWindow, attributes.windowName);
+            })
+          }
+        };
+      }
     }
+  }
 ]);
